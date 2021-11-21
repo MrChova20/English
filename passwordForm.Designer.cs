5c96fed6611e5668fa9ec9f9731b6886e17f59ed
Pablo <74419051+MrChova20@users.noreply.github.com>
2021-11-21 18:10:44 +0100

    1

---
+++ passwordForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿lnamespace FinalCountdown
+{
+    partial class passwordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(passwordForm));
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.passwordButton = new System.Windows.Forms.Button();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // passwordText
+            // 
+            this.passwordText.Location = new System.Drawing.Point(350, 238);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.Size = new System.Drawing.Size(156, 22);
+            this.passwordText.TabIndex = 0;
+            // 
+            // passwordButton
+            // 
+            this.passwordButton.Location = new System.Drawing.Point(513, 238);
+            this.passwordButton.Name = "passwordButton";
+            this.passwordButton.Size = new System.Drawing.Size(75, 23);
+            this.passwordButton.TabIndex = 1;
+            this.passwordButton.Text = "Enter";
+            this.passwordButton.UseVisualStyleBackColor = true;
+            this.passwordButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(347, 218);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(175, 17);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Enter your password here:";
+            // 
+            // passwordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.passwordButton);
+            this.Controls.Add(this.passwordText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "passwordForm";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.Button passwordButton;
+        private System.Windows.Forms.Label passwordLabel;
+    }
+}
