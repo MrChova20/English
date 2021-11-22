4bc3e13a1b5b5f19555bea5bfd0e340995937859
alvaroescr0008 <72156197+alvaroescr0008@users.noreply.github.com>
2021-11-22 12:54:47 +0100

    caca

--- PcParts/inventory.cs
+++ PcParts/inventory.cs
@@ -8,5 +8,6 @@ namespace PcParts
 {
     class inventory
     {
+
     }
 }
--- PcParts/product.cs
+++ PcParts/product.cs
@@ -8,10 +8,112 @@ namespace PcParts
 {
     class product
     {
+        /*        
         public string Name { get; internal set; }
         public object Id { get; internal set; }
         public string Description { get; internal set; }
         public object Quantity { get; internal set; }
-        public object Price { get; internal set; }
+        public double Price { get; internal set; }
+        */
+        public string name;
+
+        public int id;
+
+        public string description;
+
+        public int quantity;
+
+        public double price;
+
+        public product(string name, int id, string description, int quantity, double price)
+        {
+            this.name = name;
+            this.id = id;
+            this.description = description;
+            this.quantity = quantity;
+            this.price = price;
+        }
+        public product()
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+
+                this.name = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+
+            set
+            {
+
+                this.id = value;
+
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+
+            set
+            {
+                this.description = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return this.quantity;
+            }
+
+            set
+            {
+
+                this.quantity = value;
+
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return this.price;
+            }
+
+            set
+            {
+                this.price = value;
+            }
+        }
+
+        public void applyDiscount(double discount)
+        {
+            this.price = this.price * (1 - (discount / 100));
+        }
     }
+
+
 }
+
